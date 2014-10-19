6b875cda8112c6acad89305630857eee0c31d96b
Chris Sevilleja <chris@scotch.io>
2014-10-19 12:36:43 -0700

    changes for credentials

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: 'put-yours-here',
-    consumer_secret: 'put-yours-here',
-    access_token_key: 'put-yours-here',
-    access_token_secret: 'put-yours-here'
+    consumer_key: 'g9xpOwRQvn1Yy5c2cOip28V2B',
+    consumer_secret: 'hTP1CpviMHEJcdXpWUC8s44qHjOCAuqkpcZBKm3qtGb7Uo2GTi',
+    access_token_key: '1122906733-oiqpR1vUWWxY5EMNCUzL4Jmnlfnq72400sBcm7k',
+    access_token_secret: 'DK0Ql7QFlzFfqOhnEOgrZeQSXXMFGtLbKfQyUJdC2BVmG'
   }
 }
