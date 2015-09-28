845af14a94e8b8d2cf0f425824409f77bf1e5ede
Flavio Li Volsi <xthunderx@gmail.com>
2015-09-28 22:03:21 +0200

    Bugfix streamHandler.js
    Sometimes the tweets are malformed (currently don't know why), so the script fires a "TypeError: Cannot read property 'name' of undefined". Checking if data['user'] is defined fixes that.

--- utils/streamHandler.js
+++ utils/streamHandler.js
@@ -4,28 +4,32 @@ module.exports = function(stream, io){
 
   // When tweets get sent our way ...
   stream.on('data', function(data) {
+    
+    if (data['user'] !== undefined) {
 
-    // Construct a new tweet object
-    var tweet = {
-      twid: data['id'],
-      active: false,
-      author: data['user']['name'],
-      avatar: data['user']['profile_image_url'],
-      body: data['text'],
-      date: data['created_at'],
-      screenname: data['user']['screen_name']
-    };
-
-    // Create a new model instance with our object
-    var tweetEntry = new Tweet(tweet);
-
-    // Save 'er to the database
-    tweetEntry.save(function(err) {
-      if (!err) {
-        // If everything is cool, socket.io emits the tweet.
-        io.emit('tweet', tweet);
-      }
-    });
+      // Construct a new tweet object
+      var tweet = {
+        twid: data['id'],
+        active: false,
+        author: data['user']['name'],
+        avatar: data['user']['profile_image_url'],
+        body: data['text'],
+        date: data['created_at'],
+        screenname: data['user']['screen_name']
+      };
+  
+      // Create a new model instance with our object
+      var tweetEntry = new Tweet(tweet);
+  
+      // Save 'er to the database
+      tweetEntry.save(function(err) {
+        if (!err) {
+          // If everything is cool, socket.io emits the tweet.
+          io.emit('tweet', tweet);
+        }
+      });
+    
+    }
 
   });
 
